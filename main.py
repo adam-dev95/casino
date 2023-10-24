3bfb6e203d76027d32b6b272aba3be01a934d92c
Adam <adam.atmaniou@yahoo.com>
2023-10-24 10:34:30 +0200

    start of user sign

--- main.py
+++ main.py
@@ -1,7 +1,19 @@
 from menu import mainMenu
+from mysqlconnection import get_user, insert_user
 
 def main():
+    user = signin_user()
+    print("Bienvenue {} !".format(user[1]))
     mainMenu()
+
+def signin_user():
+    print("*  *  *  *  *  *  *  *  *  *  *  BIENVENUE DANS LE CASINO  *  *  *  *  *  *  *  *  *  *  *\n")
+    name = input("Je suis Python. Quel est votre pseudo ? \n")
+    user = get_user(name)
+    if user is None:
+        insert_user(name)
+        user = get_user(name)
+    return user
 
 if __name__ == "__main__":
     main()
